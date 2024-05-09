190dfac906f9e9b3a25bb61723c15604efa8d78f
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2024-05-09 11:25:38 +0800

    added admin header + footer

---
+++ includes/admin-footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="container">
+    &copy; Creative Folk <?= date('y'); ?>
+  </div>
+</footer>
+<script src="../js/site.js"></script>
+</body>
+</html>
---
+++ includes/admin-header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Creative Folk Admin</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
+    <link rel="shortcut icon" type="image/png" href="../img/favicon.ico">
+  </head>
+  <body>
+    <header class="header-admin">
+      <div class="container">
+        <a href="skip-link" href="#content">Skip to content</a>
+        <div class="logo">
+          <a href="../index.php"><img src="../img/logo.png" alt="Creative Folk"></a>
+        </div>
+        <nav>
+          <button id="toggle-navigation" aria-expanded="false">
+            <span class="icon-menu"></span><span class="hidden">Menu</span>
+          </button>
+          <ul id="menu">
+            <li><a href="#">Articles</a></li> // add path
+            <li><a href="#">Categories</a></li> // add path
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </body>
+</html>
