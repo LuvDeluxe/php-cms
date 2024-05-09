466bcc940ff28ddf57cab3ac01cc3d361ddb7dc3
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2024-05-09 11:26:13 +0800

    added function for redirecting

--- includes/functions.php
+++ includes/functions.php
@@ -41,3 +41,9 @@ function handle_shutdown() {
     handle_exception($e);
   }
 }
+function redirect(string $location, array $parameters = [], $response_code = 302) {
+  $qs = $parameters ? '?' . http_build_query($parameters) : '';
+  $location = $location . $qs;
+  header('Location: ' . $location, $response_code);
+  exit;
+}
