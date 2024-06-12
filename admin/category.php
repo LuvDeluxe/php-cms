3c513a4294c1553e937ba89e72be14d688066916
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2024-06-12 14:58:38 +0700

    added submit form for updating categories

--- admin/category.php
+++ admin/category.php
@@ -65,4 +65,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
   }
 }
+?>
+
+<?php include 'includes/admin-header.php'; ?>
+<main class="container admin" id="content">
+  <form action="category.php?id=<?= $id ?>" method="post" class="narrow">
+    <h2>Edit Category</h2>
+    <?php if ($errors['warning']) { ?>
+    <div class="alert alert-danger"><?= $errors['warning'] ?></div>
+    <?php } ?>
+
+    <div class="form-group">
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name"
+             value="<?= html_escape($category['name']) ?>" class="form-control">
+      <span class="errors"><?= $errors['name'] ?></span>
+    </div>
+
+    <div class="form-group">
+      <label for="description">Description: </label>
+      <textarea name="description" id="description" class="form-control">
+        <?= html_escape($category['description']) ?></textarea>
+      <span class="errors"><?= $errors['description'] ?></span>
+    </div>
+
+    <div class="form-check">
+      <input type="checkbox" name="navigation" id="navigation"
+             value="1" class="form-check-input"
+             <?= ($category['navigation'] === 1) ?'checked' : '' ?>>
+      <label class="form-check-label" for="navigation">Navigation</label>
+    </div>
+
+    <input type="submit" value="save" class="btn btn-primary btn-save">
+  </form>
+</main>
+<?php include 'includes/admin-footer.php'; ?>
 
