99e51b25c0edcdf36c7a7ccedb0790ee54a189dd
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2024-06-27 13:54:56 +0800

    add href for articles and categories in admin header

--- includes/admin-header.php
+++ includes/admin-header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Creative Folk Admin</title>
@@ -8,23 +8,21 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
     <link rel="shortcut icon" type="image/png" href="../img/favicon.ico">
-  </head>
-  <body>
-    <header class="header-admin">
-      <div class="container">
+</head>
+<body>
+<header class="header-admin">
+    <div class="container">
         <div class="logo">
-          <a href="../index.php"><img src="../img/logo.png" alt="Creative Folk"></a>
+            <a href="../index.php"><img src="../img/logo.png" alt="Creative Folk"></a>
         </div>
         <nav>
-          <button id="toggle-navigation" aria-expanded="false">
-            <span class="icon-menu"></span><span class="hidden">Menu</span>
-          </button>
-          <ul id="menu">
-            <li><a href="#">Articles</a></li>
-            <li><a href="#">Categories</a></li>
-          </ul>
+            <button id="toggle-navigation" aria-expanded="false">
+                <span class="icon-menu"></span><span class="hidden">Menu</span>
+            </button>
+            <ul id="menu">
+                <li><a href="../admin/articles.php">Articles</a></li>
+                <li><a href="../admin/categories.php">Categories</a></li>
+            </ul>
         </nav>
-      </div>
-    </header>
-  </body>
-</html>
+    </div>
+</header>
