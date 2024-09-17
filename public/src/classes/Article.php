0e63df8be2ade99a2d4a11154c2406dbf9d4bce7
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2024-09-17 11:49:33 +0800

    added getAll method

--- public/src/classes/Article.php
+++ public/src/classes/Article.php
@@ -28,4 +28,31 @@ class Article
     }
     return $this->db->runSQL($sql, [$id])->fetch();
   }
+
+  public function getAll($published = true, $category = null, $member = null, $limit = 1000): array
+  {
+    $arguments['category'] = $category;
+    $arguments['category1'] = $category;
+    $arguments['member'] = $member;
+    $arguments['member1'] = $member;
+    $arguments['limit'] = $limit;
+    $sql = "SELECT a.id, a.title, a.summary, a.category_id,
+            a.member_id, a.published,
+            c.name AS category,
+            CONCAT(m.forename, ' ', m.surname) AS author,
+            i.file AS image_file,
+            i.alt AS image_alt
+            FROM article AS a
+            JOIN category AS c ON a.category_id = c.id
+            JOIN member AS m ON a.member_id = m.id
+            LEFT JOIN image AS i on a.image_id = i.id
+            WHERE (a.category_id = :category OR :category1 IS null)
+            AND (a.member_id = :member OR member1 IS null) ";
+
+    if ($published) {
+      $sql .= "AND a.published = 1 ";
+    }
+    $sql .= "ORDER BY a.id DESC LIMIT :limit";
+    return $this->db->runSQL($sql, $arguments)->fetchAll();
+  }
 }
